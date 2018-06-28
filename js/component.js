288f04473f4cccca39bfbc70dc8d92f45be9150f
zk <775321614@qq.com>
2018-06-28 19:14:04 +0800

    0628

--- js/component.js
+++ js/component.js
@@ -1,6 +1,11 @@
 //blockly
 Vue.component('blockly-code', {
-  template: '<span>124444444</span>'
+  data: function() {
+    return {
+      seen: true
+    }
+  },
+  template: '<span v-if="seen">blockly编程</span>'
 });
 
 //登录组件
@@ -8,9 +13,25 @@ Vue.component('not-login', {
   template: '<div class="notLogin"><span class="tosign">登录</span><span class="register">注册</span></div>'
 });
 
+//画线编程
+Vue.component('path-code', {
+  data: function() {
+    return {
+      seen: false
+    }
+  },
+  template: '<span v-if="seen">画线编程</span>'
+})
 
-
-
+//代码编程
+Vue.component('write-code', {
+  data: function() {
+    return {
+      seen: false
+    }
+  },
+  template: '<span v-if="seen">代码编程</span>'
+})
 
 var login = new Vue({
   el: '.signIn'
